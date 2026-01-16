2e99116fbce21faa738803d802fd665a1981fe3a
Alfie Gormley <alfiegormley@Alfies-MacBook-Air-4.local>
2026-01-16 12:28:16 +0000

    Additions to pivot algorithm
    - Created a new function that returns the most suitable octave for mixing based on the fundamental pivot bpm
    
    - Split logic of calculate_pivot_bpm into fundamental pivot and octave functions

--- flask-server/app/services/pivot_service.py
+++ flask-server/app/services/pivot_service.py
@@ -1,6 +1,8 @@
 from fractions import Fraction
 
-def calculate_pivot_bpm(origin_bpm, destination_bpm, max_denominator = 16):
+
+
+def calculate_fundamental_pivot_bpm(origin_bpm, destination_bpm, max_denominator = 16):
 
     # Basic input validation
     if origin_bpm <= 0 or destination_bpm <= 0:
@@ -18,6 +20,25 @@ def calculate_pivot_bpm(origin_bpm, destination_bpm, max_denominator = 16):
     ratio = f"{i}:{j}"
 
     # Fundamental pivot BPM
-    pivot_bpm = round(origin_bpm / i, 1)
+    fundamental_pivot_bpm = round(origin_bpm / i, 1)
+
+    return ratio, fundamental_pivot_bpm
+
+# An octave of a bpm is doubling or halfing the tempo (BPM)
+def calculate_best_pivot_octave(origin_bpm, fundamental_pivot_bpm):
+
+    octave = fundamental_pivot_bpm
+    while octave * 2 <= origin_bpm:
+        octave *= 2
+
+    return octave
+
+# Returns the most suitable octave of the fundamental pivot bpm
+def calculate_pivot_bpm(origin_bpm, destination_bpm):
+
+    ratio, fundamental_pivot_bpm = calculate_fundamental_pivot_bpm(origin_bpm, destination_bpm)
+    octave = calculate_best_pivot_octave(origin_bpm, fundamental_pivot_bpm)
+    pivot_bpm = octave
 
     return ratio, pivot_bpm
+
